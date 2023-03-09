b8da42b95ef6c117e940af9cd609015f6ffbf980
dev5-adistech <92355731+dev5-adistech@users.noreply.github.com>
2023-03-09 18:03:27 +0530

    fileupload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,39 @@
+import { HttpResponse } from '@angular/common/http';
+import { FileService } from './file.service';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'storybook';
-  format: undefined;
-  url: undefined;
 
   storybook = new FormGroup({
-    task: new FormControl('', [Validators.required]),
+    story: new FormControl('', [Validators.required]),
     date: new FormControl('', [Validators.required]),
     image: new FormControl('', [Validators.required]),
   })
 
-  constructor(public _d: DomSanitizer) { }
-
+  format: undefined;
+  url: undefined;
+  fileToUpload: any;
+  imageUrl: any;
+  currentFile: any | File;
   imgsrc: string | undefined;
-  file: undefined;
-  // files: undefined;
+  file: any;
+  files: any;
+  selectedFiles: any | FileList;
+  msg: {} | null | undefined;
+  item: null | undefined;
+
+  constructor(private fileService: FileService) { }
+
+  deleteRow(item: any) {
+    this.story.splice(item, 1);
+  }
 
   onSubmit() {
     let value = this.storybook.value
@@ -33,11 +45,64 @@ export class AppComponent {
     this.storybook.reset();
   };
 
-  open(story: { srcElement: { story: any; }; }) {
-    const file = story.srcElement.story[0];
-    this.imgsrc = window.URL.createObjectURL(file);
+  selectFile(story: { target: { files: any; }; }) {
+    this.story = story.target.files;
+  }
+
+  upload() {
+    debugger
+    this.storybook = this.selectedFiles.item(0);
+    this.fileService.uploadFile(this.currentFile).subscribe(response => {
+      this.story.value = '';
+      if (response instanceof HttpResponse) {
+        this.msg = response.body;
+        console.log(response.body);
+      }
+    });
+  }
+
+  edit: any = '';
+  toDisplay = true;
+
+  editRow(story: any, index: any) {
+    this.edit = index
+    this.toDisplay = !this.toDisplay;
+    this.storybook.patchValue({
+      story: story.story,
+      date: story.date,
+      image: story.image
+    })
+  }
+
+  onupdate() {
+    let value = this.storybook.value
+    this.toDisplay = !this.toDisplay;
+    this.story[this.edit] = { story: value.story, date: value.date, image: value.image };
+    this.clearForm();
   }
 
   story: any = []
 
 }
+
+
+
+  // handleFileInput(file: FileList) {
+  //   this.fileToUpload = file.item(0);
+
+  //   //Show image preview
+  //   let reader = new FileReader();
+  //   reader.onload = (event: any) => {
+  //     this.imageUrl = event.target.result;
+  //   }
+  //   reader.readAsDataURL(this.fileToUpload);
+  // }
+
+  // onChange(story: { target: { files: (string | undefined)[]; }; }) {
+  //   this.files = story.target.files[0];
+  // }
+
+  // open(story: { srcElement: { files: any[]; }; }) {
+  //   const file = story.srcElement.files[0];
+  //   this.imgsrc = window.URL.createObjectURL(file);
+  // }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +12,7 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     PdfViewerModule
